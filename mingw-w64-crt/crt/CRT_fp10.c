eb96e8d760cfd41dd8071a45c44b6dc1fb333d4c
Billy Laws <blaws05@gmail.com>
2023-09-14 20:59:41 +0100

    CRT_fp10: Use aarch64 fenv function variants for ARM64EC

--- mingw-w64-crt/crt/CRT_fp10.c
+++ mingw-w64-crt/crt/CRT_fp10.c
@@ -11,7 +11,7 @@ void _fpreset (void)
 #if defined(_ARM_) || defined(__arm__)
   __asm__ __volatile__ (
     "vmsr	fpscr, %0\n\t" : : "r"(0 /* INITIAL_FPSCR */));
-#elif defined(_ARM64_) || defined(__aarch64__)
+#elif defined(_ARM64_) || defined(__aarch64__) || defined(__arm64ec__)
   __asm__ __volatile__ (
     "msr	fpcr, %0\n\t" : : "r"(0LL /* INITIAL_FPSCR */));
 #else
