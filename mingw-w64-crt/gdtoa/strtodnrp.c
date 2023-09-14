85c9311785a2a5626271ad76bcc999a758ab18a2
Billy Laws <blaws05@gmail.com>
2023-09-14 21:35:14 +0100

    strtopx: Use aarch64 function variant for ARM64EC

--- mingw-w64-crt/gdtoa/strtodnrp.c
+++ mingw-w64-crt/gdtoa/strtodnrp.c
@@ -86,7 +86,8 @@ __mingw_strtod (const char * __restrict__ src, char ** __restrict__ endptr)
   __attribute__((alias("__strtod")));
 
 #if !(defined(_AMD64_) || defined(__x86_64__) || \
-  defined(_X86_) || defined(__i386__))
+  defined(_X86_) || defined(__i386__)) || \
+  defined(_ARM64EC_) || defined(__arm64ec__)
 /* For systems other than x86, where long double == double, provide the
  * long double functions as aliases to __strtod. */
 
